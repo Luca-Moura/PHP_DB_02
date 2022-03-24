7beae362b26d2a5061c4a6ff62d7ed16d7bae222
Luca <luca.mm01@gmail.com>
2022-03-23 21:11:06 -0300

    Aula PHP 12 Prof1

--- lista.php
+++ lista.php
@@ -14,8 +14,31 @@ SQL;
 // Executar a query e retorna dados na variável
 $res = $conn->query($sql);
 
+echo <<<HTML
+
+<table>
+<tr>
+    <th>Nome</th>
+    <th>E-mail</th>
+    <th>Assunto</th>
+    <th>Mensagem</th>
+</tr>
+
+HTML;
+
 while ($user = $res->fetch_assoc()) :
 
-    echo '<a href="read_one.php?id=' . $user['id'] . '" target="_blank">' . $user['nome'] . '</a><br>';
+    echo <<<HTML
+    
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>
+    
+HTML;
 
 endwhile;
+
+echo '</table>';
